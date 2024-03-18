41969e00c729f063bfdb0829c150a8ac4eeb3390
Rahul Shrestha <161265688+rashres@users.noreply.github.com>
2024-03-17 21:34:47 -0500

    Update contactForm.js

--- pages/api/contactForm.js
+++ pages/api/contactForm.js
@@ -16,8 +16,8 @@ export default async function handler(req, res) {
       color: 4310753,
       timestamp: new Date().toISOString(),
       footer: {
-        icon_url: "https://yok.dev/favicon.ico",
-        text: "yok.dev",
+        icon_url: "https://shres.dev/favicon.ico",
+        text: "shres.dev",
       },
       fields: fields,
     },
